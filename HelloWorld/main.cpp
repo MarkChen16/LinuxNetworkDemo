1b839fde80a2c5fb9a468321040f02bdf799a488
chenguiquan <chenguiquan@wps.cn>
2020-05-20 11:34:25 +0800

    优化HelloWorld，修改中文乱码问题

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,8 +1,11 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main(void)
 {
 	printf("Hello, world!\n");
+
+	//中文会引起乱码，linux使用utf8编码，但windows使用的是GBK，要把全部源代码修改为Unicode无签名，unix行尾；
+	printf("你好，世界！\n");
 
 	return 0;
 }
