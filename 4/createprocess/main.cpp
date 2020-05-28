8f79ff75ce438af3df12c1abc53735aff55b0066
guiquanchen <guiquanchen@qq.com>
2020-05-28 12:14:28 +0800

    添加createprocess工程

--- 4/createprocess/main.cpp
+++ 4/createprocess/main.cpp
@@ -6,6 +6,9 @@
 fork()：复制父进程，不同的进程ID，写时复制
 system()：新建子进程，调用shell外部命令，通过调用fork()、execve()和waitpid()实现
 exec()：新建进程代替原进程，使用原进程的资源，进程ID也一样。
+
+除了init进程，所有进程都是有父进程的，所有进程都是复制或者克隆的
+使用pstree查看所有进程的父子关系
 */
 
 
@@ -48,7 +51,7 @@ void do_fork()
 	//打印父进程的ID
 	printf("current pid = %d\n", getpid());
 
-	for (int start = 0; start < 10; start++)
+	for (int start = 0; start < 1000; start++)
 	{
 		pid_t pid = fork();
 		if (pid > 0)
@@ -61,9 +64,9 @@ void do_fork()
 			//子进程：处理任务，start变量是复制父进程的内存副本
 			int i = 0;
 
-			printf("pid(%d)ppid(%d):", getpid(), getppid());
+			printf("pid(%d) ppid(%d):", getpid(), getppid());
 
-			for (i = start; i < 10; i++)
+			for (i = start; i < 1000; i++)
 			{
 				printf("%d ", i);
 			}
