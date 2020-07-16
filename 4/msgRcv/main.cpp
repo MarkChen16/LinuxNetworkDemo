1d2c75b644b772c590c56bd89deb33711305391d
guiquanchen <guiquanchen@qq.com>
2020-07-16 17:54:51 +0800

    优化程序

--- 4/msgRcv/main.cpp
+++ 4/msgRcv/main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <memory.h>
+#include <assert.h>
 
 #include <unistd.h>
 
@@ -12,33 +13,70 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-//消息类型
-#define MSGTYPE_SAYHI 11
+//查询消息队列数量、单条消息大小和消息队列消息数量限制
+//ipcs -l	显示ipc资源的限制
+//ipcs -u	显示ipc资源已使用情况
+//修改ipc资源的最大限制 /etc/sysctl.conf，重启后生效
 
-//消息定义
+//消息类型定义
+enum MsgType
+{
+	None,
+	Hello,
+	Query,
+	GoodByte,
+	Exit
+};
+
+//消息体定义
+struct msg_hello_t
+{
+	char sender[10];
+	char message[256];
+};
+
+struct msg_query_t
+{
+	char sender[10];
+	int queryID;
+};
+
+struct msg_goodbye_t 
+{
+	char sender[10];
+};
+
+//消息数据结构
 struct msgbuff_t
 {
+	union msg_t
+	{
+		msg_hello_t helloMsg;
+		msg_query_t queryMsg;
+		msg_goodbye_t goodbyeMsg;
+	};
+
 	msgbuff_t()
 	{
-		mtype = 1;
-		memset(message, 0, 256);
-		memset(sendpid, 0, 10);
+		mtype = None;
+		memset(&msg, 0, sizeof(msg_t));
+
+		assert(sizeof(msg_t) < 8192);
 	}
 
 	//消息固定变量
 	long mtype;
 
-	//自定义部分
-	char message[256];
-	char sendpid[10];
+	//自定义部分(最大长度为8192)
+	msg_t msg;
 };
 
 int main(int argc, char* argv[])
 {
 	int ret = 0;
 
+	//根据文件的inode值的唯一性和计划编号生成唯一的ipc键值，文件路径必须存在，进程之间IPC过程中，文件不能删除或者重新创建，否则inode会改变；
 	system("touch /tmp/msg");
-
 	key_t ipcKey = ftok("/tmp/msg", 'a');
 	if (ipcKey == -1)
 	{
@@ -47,7 +85,8 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		int msqID = msgget(ipcKey, IPC_CREAT | 0666);	//IPC_PRIVATE表示系统生成一个
+		//ipcKey也可以为IPC_PRIVATE，表示是一个私有IPC，仅在父子进程之间使用
+		int msqID = msgget(ipcKey, IPC_CREAT | 0666);
 		if (msqID == -1)
 		{
 			printf("Failed to msgget.\n");
@@ -75,9 +114,10 @@ int main(int argc, char* argv[])
 					printf("msg count: %d\n", msg_stat.msg_qnum);
 				}
 
-				//阻塞读取消息，一直等到有消息才返回
-				struct msgbuff_t msgHi;
-				result = msgrcv(msqID, &msgHi, sizeof(struct msgbuff_t), MSGTYPE_SAYHI, IPC_NOWAIT);
+				//__msgtyp为0时，读取消息队列中第一条消息返回；大于0时，读取消息队列中第一条类型为msgtype的消息返回；
+				//__msgflg默认阻塞读取消息，一直等到有消息才返回，设置为IPC_NOWAIT，没有消息则马到返回错误
+				struct msgbuff_t msgData;
+				result = msgrcv(msqID, &msgData, sizeof(struct msgbuff_t), 0, 0);
 				if (result == -1)
 				{
 					printf("Failed to msgrcv.\n");
@@ -87,7 +127,27 @@ int main(int argc, char* argv[])
 				}
 				else
 				{
-					printf("Message(%s): %s\n", msgHi.sendpid, msgHi.message);
+					switch (msgData.mtype)
+					{
+					case MsgType::Hello:
+						printf("%s login: %s\n", msgData.msg.helloMsg.sender, msgData.msg.helloMsg.message);
+						break;
+					case MsgType::Query:
+						printf("%s query id: %i\n", msgData.msg.queryMsg.sender, msgData.msg.queryMsg.queryID);
+						break;
+					case MsgType::GoodByte:
+						printf("%s say goodbye.\n", msgData.msg.goodbyeMsg.sender);
+						break;
+					case MsgType::Exit:
+						printf("system exit.\n");
+						break;
+					default:
+						printf("unknown message type is %i.", msgData.mtype);
+						break;
+					}
+
+					if (msgData.mtype == MsgType::Exit)
+						break;
 				}
 			}
 		}
--- 4/msgSend/main.cpp
+++ 4/msgSend/main.cpp
@@ -2,6 +2,7 @@
 #include <memory.h>
 #include <string.h>
 #include <iostream>
+#include <assert.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,6 +12,8 @@
 #include <sys/msg.h>
 
 #include <unistd.h>
+#include <signal.h>
+#include <wait.h>
 
 /*
 消息：不同进程之间通信
@@ -18,34 +21,69 @@
 相关函数：ftok  msgget  msgctl  msgsnd  msgrcv
 */
 
-//消息类型
-#define MSGTYPE_SAYHI 11
+//消息类型定义
+enum MsgType
+{
+	None,
+	Hello,
+	Query,
+	GoodByte,
+	Exit
+};
 
-//消息定义
+//消息体定义
+struct msg_hello_t
+{
+	char sender[10];
+	char message[256];
+};
+
+struct msg_query_t
+{
+	char sender[10];
+	int queryID;
+};
+
+struct msg_goodbye_t
+{
+	char sender[10];
+};
+
+//消息数据结构
 struct msgbuff_t
 {
+	union msg_t
+	{
+		msg_hello_t helloMsg;
+		msg_query_t queryMsg;
+		msg_goodbye_t goodbyeMsg;
+	};
+
 	msgbuff_t()
 	{
-		mtype = 1;
-		memset(message, 0, 256);
-		memset(sendpid, 0, 10);
+		mtype = None;
+		memset(&msg, 0, sizeof(msg_t));
+
+		assert(sizeof(msg_t) < 8192);
 	}
 
 	//消息固定变量
 	long mtype;
-	
+
 	//自定义部分(最大长度为8192)
-	char message[256];
-	char sendpid[10];
+	msg_t msg;
 };
+
+void sendMsg(int msqId, msgbuff_t& msgBuff);
 
 int main(int argc, char* argv[])
 {
 	int ret = 0;
 
+	signal(SIGCHLD, SIG_IGN);
+
+	//根据文件的inode值的唯一性和计划编号生成唯一的ipc键值，文件路径必须存在，进程之间IPC过程中，文件不能删除或者重新创建；
 	system("touch /tmp/msg");
-	
-	//根据文件的inode值的唯一性和计划编号生成唯一的ipc和键值，文件路径必须存在，进程之间IPC过程中，文件不能删除或者重新创建；
 	key_t ipcKey = ftok("/tmp/msg", 'a');
 	if (ipcKey == -1)
 	{
@@ -58,36 +96,70 @@ int main(int argc, char* argv[])
 		//创建无模式：如果不存在则出错，如果存在则引用
 		//IPC_CREAT：如果不存在则创建，如果存在则引用
 		//IPC_CREAT | IPC_EXCL：如果不存在则创建，如果存在则报错EEXIST
-		int msqID = msgget(ipcKey, IPC_CREAT | 0666);	//IPC_PRIVATE表示系统生成一个
-		if (msqID == -1)
+		int msqId = msgget(ipcKey, IPC_CREAT | 0666);
+		if (msqId == -1)
 		{
 			printf("Failed to msgget.\n");
 			ret = 2;
 		}
 		else
 		{
-			//发送消息
-			struct msgbuff_t msgHi;
-			msgHi.mtype = MSGTYPE_SAYHI;
-			strcpy(msgHi.message, "Hi, Mark Chen!");
-
-			int pid = getpid();
-			strcpy(msgHi.sendpid, std::to_string(pid).c_str());
-
-			//IPC_NOWAIT：消息队列已满时，直接返回不写入队列
-			int len = sizeof(struct msgbuff_t) - sizeof(long);
-			int result = msgsnd(msqID, &msgHi, len, IPC_NOWAIT);
-			if (result == -1)
+			for (int i = 0; i < 100000; i++)
 			{
-				printf("Failed to msgsnd.\n");
-				ret = 2;
+				pid_t pid = fork();
+				if (pid == 0)
+				{
+					//发送hello消息
+					struct msgbuff_t helloMsgBuff;
+					helloMsgBuff.mtype = MsgType::Hello;
+					strcpy(helloMsgBuff.msg.helloMsg.sender, std::to_string(i).c_str());
+					strcpy(helloMsgBuff.msg.helloMsg.message, "Hi, Mark Chen!");
+
+					sendMsg(msqId, helloMsgBuff);
+
+					//发送query消息
+					struct msgbuff_t queryMsgBuff;
+					queryMsgBuff.mtype = MsgType::Query;
+					strcpy(queryMsgBuff.msg.queryMsg.sender, std::to_string(i).c_str());
+					queryMsgBuff.msg.queryMsg.queryID = i;
+
+					sendMsg(msqId, queryMsgBuff);
+
+					//发送goodbye消息
+					struct msgbuff_t goodbyeMsgBuff;
+					goodbyeMsgBuff.mtype = MsgType::GoodByte;
+					strcpy(goodbyeMsgBuff.msg.goodbyeMsg.sender, std::to_string(i).c_str());
+
+					sendMsg(msqId, goodbyeMsgBuff);
+
+					exit(0);
+				}
 			}
-			else
-			{
-				printf("Send Hi Message.\n");
-			}
+
+			//等待所有子进程结束，发送exit消息
+			while (wait(NULL) != -1) {};
+
+			struct msgbuff_t exitMsgBuff;
+			exitMsgBuff.mtype = MsgType::Exit;
+
+			sendMsg(msqId, exitMsgBuff);
 		}
 	}
 
 	return ret;
+}
+
+void sendMsg(int msqId, msgbuff_t& msgBuff)
+{
+	//msgflag默认为0，写入后返回
+	//IPC_NOWAIT：消息队列已满时，直接返回不写入队列
+	int result = msgsnd(msqId, &msgBuff, sizeof(msgbuff_t) - sizeof(long), 0);
+	if (result == -1)
+	{
+		printf("Failed to msgsnd.\n");
+	}
+	else
+	{
+		//printf("success to msgsnd.\n");
+	}
 }
